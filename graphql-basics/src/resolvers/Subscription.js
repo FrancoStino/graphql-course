f8405b456c35afb3f92a57b837f76adcd7a982f6
Davide <info@davideladisa.it>
2025-01-06 21:32:02 +0100

    feat(graphql): ✨ add `post` subscription and publish new post data
    - Implemented a subscription for new posts.
    - Set up the resolver for the `post` subscription without arguments.
    - Modified the post creation mutation to publish new post data only if the post is marked as "published".

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -1,12 +1,3 @@
-// Goal: Create  a subscription for a new posts
-
-// 1. Define "post" subscription. No arguments are necessary. Response should be a post object.
-// 2. Setup the resolver for post. Since there are no args, a channel name like "post" is fine.
-// 3. Modify the mutation for creating a post to publish the new post data.
-//     - Only call pubsub.publish if the post had "published" set to true.
-//     - Don't worry about updatePost or deletePost
-// 4. Test your work.
-
 const Subscription = {
     comment: {
         subscribe(parent, { postId }, { db, pubsub }, info) {
